75f91ea2ce97d397158b5f721a102b770619e51a
tina nakova <tinna.nakova@gmail.com>
2016-10-16 02:56:23 +0300

    Update Displayer.java
    added displaying functionality

--- src/hangman/Displayer.java
+++ src/hangman/Displayer.java
@@ -25,7 +25,119 @@ public class Displayer {
 		System.out.print("\n");
 	}
 	
-	static public void displayMistakes(int mistakesCount) {
-		System.out.println("Mistakes: " + mistakesCount);
+	static public void displayStickFigure(int mistakesCount) {
+		StringBuilder stickman = new StringBuilder();
+		switch (mistakesCount) {
+		case 1: stickman.append("                   \n"); 
+				stickman.append("                   \n");
+				stickman.append("                   \n");
+				stickman.append("                   \n");
+				stickman.append("                   \n");
+				stickman.append("                   \n");
+				stickman.append("                   \n");
+				stickman.append("                   \n");
+				stickman.append("_________          \n");
+				break;
+		
+		case 2: stickman.append("                   \n"); 
+				stickman.append(" |                 \n");
+				stickman.append(" |                 \n");
+				stickman.append(" |                 \n");
+				stickman.append(" |                 \n");
+				stickman.append(" |                 \n");
+				stickman.append(" |                 \n");
+				stickman.append(" |                 \n");
+				stickman.append("_|_______          \n");
+				break;
+		
+		case 3: stickman.append(" ________________  \n"); 
+				stickman.append(" |                 \n");
+				stickman.append(" |                 \n");
+				stickman.append(" |                 \n");
+				stickman.append(" |                 \n");
+				stickman.append(" |                 \n");
+				stickman.append(" |                 \n");
+				stickman.append(" |                 \n");
+				stickman.append("_|_______          \n");
+				break;
+		
+		case 4: stickman.append(" ________________  \n"); 
+				stickman.append(" |         |       \n");
+				stickman.append(" |                 \n");
+				stickman.append(" |                 \n");
+				stickman.append(" |                 \n");
+				stickman.append(" |                 \n");
+				stickman.append(" |                 \n");
+				stickman.append(" |                 \n");
+				stickman.append("_|_______          \n");
+				break;
+
+		case 5: stickman.append(" ________________  \n"); 
+				stickman.append(" |         |       \n");
+				stickman.append(" |        ( )      \n");
+				stickman.append(" |                 \n");
+				stickman.append(" |                 \n");
+				stickman.append(" |                 \n");
+				stickman.append(" |                 \n");
+				stickman.append(" |                 \n");
+				stickman.append("_|_______          \n");
+				break;
+		
+		case 6: stickman.append(" ________________  \n"); 
+				stickman.append(" |         |       \n");
+				stickman.append(" |        ( )      \n");
+				stickman.append(" |         |       \n");
+				stickman.append(" |         |       \n");
+				stickman.append(" |                 \n");
+				stickman.append(" |                 \n");
+				stickman.append(" |                 \n");
+				stickman.append("_|_______          \n");
+				break;
+		
+		case 7: stickman.append(" ________________  \n"); 
+				stickman.append(" |         |       \n");
+				stickman.append(" |        ( )      \n");
+				stickman.append(" |        /|       \n");
+				stickman.append(" |         |       \n");
+				stickman.append(" |                 \n");
+				stickman.append(" |                 \n");
+				stickman.append(" |                 \n");
+				stickman.append("_|_______          \n");
+				break;
+		
+		case 8: stickman.append(" ________________  \n"); 
+				stickman.append(" |         |       \n");
+				stickman.append(" |        ( )      \n");
+				stickman.append(" |        /|\\     \n");
+				stickman.append(" |         |       \n");
+				stickman.append(" |                 \n");
+				stickman.append(" |                 \n");
+				stickman.append(" |                 \n");
+				stickman.append("_|_______          \n");
+				break;
+				
+		case 9: stickman.append(" ________________  \n"); 
+				stickman.append(" |         |       \n");
+				stickman.append(" |        ( )      \n");
+				stickman.append(" |        /|\\     \n");
+				stickman.append(" |         |       \n");
+				stickman.append(" |        /        \n");
+				stickman.append(" |                 \n");
+				stickman.append(" |                 \n");
+				stickman.append("_|_______          \n");
+				break;
+			
+		case 10: stickman.append(" ________________  \n"); 
+				stickman.append(" |         |       \n");
+				stickman.append(" |        ( )      \n");
+				stickman.append(" |        /|\\     \n");
+				stickman.append(" |         |       \n");
+				stickman.append(" |        / \\     \n");
+				stickman.append(" |                 \n");
+				stickman.append(" |                 \n");
+				stickman.append("_|_______          \n");
+				break;
+		}
+		System.out.println(stickman);
 	}
 }
